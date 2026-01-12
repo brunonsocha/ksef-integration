21e74bd978b1d4e18b42b092b627a26628da747e
BrunonSocha <brunon.socha@gmail.com>
2026-01-12 14:30:20 +0100

    should receive a session token now. ready to create the login function and store the session token in the client

--- internal/ksef/auth.go
+++ internal/ksef/auth.go
@@ -1,6 +1,7 @@
 package ksef
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -12,6 +13,23 @@ type ChallengeResponse struct {
 	Challenge string `json:"challenge"`
 	Timestamp time.Time `json:"timestamp"`
 	TimestampMs int64 `json:"timestampMs"`
+}
+
+type AuthenticationPayload struct {
+	Challenge string `json:"challenge"`
+	ContextIdentifier struct {
+		Type string `json:"type"`
+		Value string `json:"value"`
+	} `json:"contextIdentifier"`
+	EncryptedToken string `json:"encryptedToken"`
+}
+
+type AuthenticationResponse struct {
+	ReferenceNumber string `json:"referenceNumber"`
+	AuthenticationToken struct {
+		Token string `json:"token"`
+		ValidUntil time.Time `json:"validUntil"`
+	} `json:"authenticationToken"`
 }
 
 // i'm sure this code can be prettier. will refactor.
@@ -44,3 +62,50 @@ func (c *Client) getChallenge() (*ChallengeResponse, error) {
 	}
 	return &cha, nil
 }
+
+func (c *Client) startSession(encryptedToken string, cha *ChallengeResponse) (*AuthenticationResponse, error) {
+	posturl := c.ApiURL + "/auth/ksef-token"
+	payload := AuthenticationPayload{
+		Challenge: cha.Challenge,
+		// anonymous struct used here, didn't want to pollute the top of the file with an additional struct to nest within another one
+		ContextIdentifier: struct {
+			Type string `json:"type"`
+			Value string `json:"value"`
+		}{
+			Type: "Nip",
+			Value: c.NIP,
+		},
+		EncryptedToken: encryptedToken,
+	}
+	payloadJson, err := json.Marshal(payload)
+	if err != nil {
+		return nil, err
+	}
+	r, err := http.NewRequest("POST", posturl, bytes.NewBuffer(payloadJson))
+	if err != nil {
+		return nil, err
+	}
+	r.Header.Set("Accept", "application/json")
+	r.Header.Set("Content-Type", "application/json")
+	response, err := c.httpClient.Do(r)
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusAccepted {
+		return nil, fmt.Errorf("Wystapił błąd - KSeF nie zaakceptował tokena - %d", response.StatusCode)
+	}
+	var authRes AuthenticationResponse
+	responseJson, err := io.ReadAll(response.Body)
+	if err != nil {
+		return nil, fmt.Errorf("Nie można było odczytać odpowiedzi.")
+	}
+	err = json.Unmarshal(responseJson, &authRes)
+	if err != nil {
+		return nil, fmt.Errorf("Nie można było odczytać odpowiedzi.")
+	}
+	return &authRes, nil
+}
+
+
